9099996db3b1345817fbbe2d6665b3d8dcbc2d1f
AMD-hub <aelyousefi@insea.ac.ma>
2024-06-25 15:46:52 +0100

    adding setup

---
+++ steup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='amd_pack',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    description='A brief description of the package',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/AMD-hub/amd_pack',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
